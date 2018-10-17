16af471d7f8a517139fc5251bdfefa66363060dc
Iliesi_Andrew <iliea766@hsd.k12.or.us>
2018-10-17 15:28:58 -0700

    adding player in platform objects

---
+++ Assets/Scripts/playercontoller.cs
@@ -0,0 +1,13 @@
+﻿1private Rigidbody m_rb;
+2
+3// Use this for initialization
+4void Start()
+5{
+6 m_rb = GetComponent<Rigidbody>();
+7}
+8
+9void FixedUpdate()
+10{
+11 float movement = Input.GetAxis("Horizontal");
+12 m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+13}
