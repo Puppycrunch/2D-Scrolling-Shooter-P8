4fb0a908711e2b57fd7f5ec4d005678d83fe428c
Iliesi_Andrew <iliea766@hsd.k12.or.us>
2018-11-01 15:05:55 -0700

    adding speed multiplier and maximum speed value

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody m_rb;
-
+    public float speed = 10.0F;
+    public float max_speed = 12.0F;
     // Use this for initialization
     private void Start()
     {
         m_rb = GetComponent<Rigidbody>();
     }
-    void Update()
+    private void FixedUpdate()
     {
         float movement = Input.GetAxis("Horizontal");
-        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+        m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
+        m_rb.velocity = new Vector3(
+        Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),
+        m_rb.velocity.y,m_rb.velocity.z);
     }
 }
  
