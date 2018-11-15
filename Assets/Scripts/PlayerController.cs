56c788d0f19963e6d679a9e6b58c0e643a900e1f
Puppycrunch <iliea766@hsd.k12.or.us>
2018-11-15 15:17:57 -0800

    adding pickup prefab and pickup mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,23 +12,30 @@ public class PlayerController : MonoBehaviour
     private float collider_radius = 0.0F;
     public float grounded_epsilon = 0.05F;
     public int user_layer_platform;
+    private float get_axis_horizontal = 0.0F;
+    private bool get_key_down_space = false;
+    public string pickup_tag;
+
     // Use this for initialization
-    private void Start()
-    {
+    private void Start() {
         m_rb = GetComponent<Rigidbody>();
         m_collider = GetComponent<Collider>();
         collider_radius = m_collider.bounds.extents.y;
     }
+    void Update() {
+        get_axis_horizontal = Input.GetAxis("Horizontal");
+        get_key_down_space = Input.GetKey(KeyCode.Space);
+    }
     private void FixedUpdate()
     {
-        float movement = Input.GetAxis("Horizontal");
+        float movement = get_axis_horizontal;
         m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
         m_rb.velocity = new Vector3(
         Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),
         m_rb.velocity.y,m_rb.velocity.z
         );
 
-        if ( Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        if ( get_key_down_space && isGrounded())
             m_rb.AddForce(0.0F, jump_height, 0.0F);
     }
     bool isGrounded()
@@ -40,5 +47,11 @@ public class PlayerController : MonoBehaviour
             collider_radius + grounded_epsilon,
             platform_layer);
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(pickup_tag))
+            other.gameObject.SetActive(false);
+    }
+
 }
  
